646fcbcc9398d4afc64faf90a1ea2da49cc86f88
JJX <jjx@speluncula.de>
2018-01-01 13:50:48 +0100

    Create adsb.sh

---
+++ scripts/adsb.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+#place this script in /opt/adsb and chmod +x it
+while true; do
+curl -s -u username:password -X POST -H "Content-type: application/json" -d @/run/dump1090-mutability/aircraft.json https://airlog.chaos-consulting.de/aircraftin/index.php
+sleep 1;
+done;
