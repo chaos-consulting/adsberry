28b52a86ccd2f84cb481972aed9157ac49f11c44
JJX <10248961+jjx-@users.noreply.github.com>
2021-06-19 13:33:00 +0200

    Update adsb.sh

--- scripts/adsb.sh
+++ scripts/adsb.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 #place this script in /opt/adsb and chmod +x it
+#Edit username an password here
+username='yourusernamehere';
+password='yourpasswordhere';
+#No changes needed from here on
 while true; do
-curl -s -u username:password -X POST -H "Content-type: application/json" -d @/run/dump1090-mutability/aircraft.json https://adsb.chaos-consulting.de/aircraftin/index.php
+curl -s -u $username:$password -X POST -H "Content-type: application/json" -d @/run/dump1090-mutability/aircraft.json https://adsb.chaos-consulting.de/aircraftin/index.php
 sleep 1;
 done;
