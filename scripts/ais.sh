4d19cb917e067183e3a144f19f3d946b96540c93
skymaker-c2is <63470599+skymaker-c2is@users.noreply.github.com>
2021-10-06 21:23:50 +0200

    Update ais.sh

--- scripts/ais.sh
+++ scripts/ais.sh
@@ -14,7 +14,7 @@ do
 		msgs+=$(echo "$x" | tr -d '\r\n')
 		msgs+=","
 	fi
-	# if more than 1 second elapsed and the cache is not empty and the last curl was succesful, post the cached msgs to the server
+	# if more than 1 second elapsed and the cache is not empty, post the cached msgs to the server
 	if (( $SECONDS > 0 )) && [[ ! -z "${msgs}" ]]; then
 		echo "[ ${msgs%,*} ]" | gzip | curl -s -u $username:$password -X POST -H "Content-Type: application/json" -H "Content-encoding: gzip" --data-binary @- -m 1 https://ais.chaos-consulting.de/shipin/index.php & 
 		SECONDS=0
